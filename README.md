0f70087e6f184e342d7fbc88a12e683a3adc17be
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-07-15 21:11:00 +0800

    Fly in experience animation

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # WHYELAB_UIUX-Portfolio
 
-This is my website for introducing myself. I would include some details of me and experience I have. 
+My UIUX Portfolio.
 
-# Icons
+## Icons for Demo
 
 Linkedin: https://www.flaticon.com/free-icon/linkedin_174857
 
--- production/src/sections/WorkExperience.tsx
+++ production/src/sections/WorkExperience.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, Card, CardContent } from '@mui/material';
 
 const WorkExperience = () => {
+    const [showAnimation, setShowAnimation] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+        const experienceSection = document.getElementById('experience-section');
+        if (experienceSection) {
+            const rect = experienceSection.getBoundingClientRect();
+            setShowAnimation(rect.top <= window.innerHeight);
+        }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const experiences = [
+        // Add your work experiences here
+        {
+        company: 'Company A',
+        title: 'Job Title A',
+        date: 'Date A',
+        description: 'Description of Job A',
+        },
+        {
+        company: 'Company B',
+        title: 'Job Title B',
+        date: 'Date B',
+        description: 'Description of Job B',
+        },
+        // Add more experiences if needed
+    ];
+
     return (
-        <Box>
-            <Typography variant="h4" color="primary">Work Experience</Typography>
-            <Box>
-                <Typography variant="body1">Company Name</Typography>
-                <Typography variant="body2">Job Title - Date</Typography>
-                <Typography variant="body2">Description of your role and responsibilities</Typography>
-            </Box>
+        <Box id="experience-section">
+        <Typography variant="h4" color="primary" sx={{ marginBottom: '1rem' }}>Work Experience</Typography>
+        {experiences.map((experience, index) => (
+            <Card
+            key={index}
+            sx={{
+                marginBottom: '1rem',
+                marginLeft: showAnimation && index % 2 === 0 ? '0' : 'auto', // Align left for even indices
+                marginRight: showAnimation && index % 2 === 1 ? '0' : 'auto', // Align right for odd indices
+                transform: showAnimation ? 'translateX(0)' : `translateX(${index % 2 === 0 ? '-100%' : '100%'})`, // Slide in from left/right
+                transition: 'transform 0.5s ease',
+            }}
+            >
+            <CardContent>
+                <Typography variant="h6">{experience.company}</Typography>
+                <Typography variant="body1">{experience.title}</Typography>
+                <Typography variant="body2">{experience.date}</Typography>
+                <Typography variant="body2">{experience.description}</Typography>
+            </CardContent>
+            </Card>
+        ))}
         </Box>
     );
 };
