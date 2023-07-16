d965fbd17a02fc00a85d6426f2bc1c2fa600fd18
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-07-16 20:26:53 +0800

    Progress bar theme styling

--- production/src/sections/TechnicalSkill.tsx
+++ production/src/sections/TechnicalSkill.tsx
@@ -9,29 +9,43 @@ const TechnicalSkill = forwardRef<HTMLDivElement>((props, ref) => {
         { name: 'HTML', percentage: 80 },
         { name: 'CSS', percentage: 75 },
         { name: 'UI/UX Design', percentage: 70 },
-        // Add more technical skills here
     ];
 
     return (
         <div ref={ref}>
-            <Box>
+            <Box sx={{ mb: 4 }}>
                 <Typography variant="h4" color="primary">Technical Skills</Typography>
-
-                <Grid container spacing={2}>
-
+            </Box>
+                
+                <Grid container spacing={10}>
                 {technicalSkills.map(skill => (
                     <Grid item xs={12} sm={6} md={4} key={skill.name}>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography>{skill.name}</Typography>
-                        <Box sx={{ width: '100%', ml: 1 }}>
-                            <LinearProgress variant="determinate" value={skill.percentage} />
+                    <Box
+                        sx={{
+                            backgroundColor: 'green',
+                            p: 2,
+                            borderRadius: 8,
+                            textAlign: 'center',
+                            color: 'white',
+                        }}
+                    >
+                        <Typography variant="h6">{skill.name}</Typography>
+                        <Box sx={{ mt: 1 }}>
+                        <LinearProgress
+                            variant="determinate"
+                            value={skill.percentage}
+                            sx={{
+                                backgroundColor: 'grey',
+                                '& .MuiLinearProgress-bar': {
+                                    backgroundColor: 'blue'
+                                }
+                            }}
+                        />
                         </Box>
                     </Box>
                     </Grid>
                 ))}
-
                 </Grid>
-            </Box>
         </div>
     );
 });
