cfe2f5fa58a5ba1b4b55d7cd568bdbaca92be3ee
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-07-16 21:43:10 +0800

    Icons for about me

--- production/src/sections/AboutMe.tsx
+++ production/src/sections/AboutMe.tsx
@@ -1,5 +1,9 @@
 import React, { forwardRef } from 'react';
-import { Box, Typography } from '@mui/material';
+import { IconButton, Stack, Typography } from '@mui/material';
+import { GitHub } from '@mui/icons-material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 import profileImage from '../profile.jpg';
 
@@ -31,6 +35,45 @@ const AboutMe = forwardRef<HTMLDivElement>((props, ref) => {
                 <Typography variant="body1">
                     I am a final year Computer Engineering Undergraduate @NUS
                 </Typography>
+
+                <br/>
+                <div 
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Stack direction="row">
+                        <IconButton 
+                            href={"https://github.com/YEOWEIHNGWHYELAB"} 
+                            target="_blank"
+                            style={{ padding: '12px' }}
+                        >
+                            <GitHub  style={{ fontSize: '4rem' }} />
+                        </IconButton>
+                        <IconButton 
+                            href={"https://www.linkedin.com/in/yeo-wei-hng/"} 
+                            target="_blank"
+                            style={{ padding: '12px' }}
+                        >
+                            <LinkedInIcon style={{ fontSize: '4rem' }} />
+                        </IconButton>
+                        <IconButton 
+                            href={"https://www.youtube.com/@WHYELAB/"} 
+                            target="_blank"
+                            style={{ padding: '12px' }}
+                        >
+                            <YouTubeIcon style={{ fontSize: '4rem' }} />
+                        </IconButton>
+                        <IconButton 
+                            href={"https://docs.google.com/document/d/1DSQR4uLtwMjjSeLXZhUCq2UGAH-gTtxo/edit?usp=sharing&ouid=116482923554621042379&rtpof=true&sd=true"} 
+                            target="_blank"
+                            style={{ padding: '12px' }}
+                        >
+                            <AccountBoxIcon style={{ fontSize: '4rem' }} />
+                        </IconButton>
+                    </Stack>
+                </div>
             </div>
         </div>
     );
