fdf1c20f16a6d8ffdf3a98f89629e7631dd0cd98
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-07-16 20:22:13 +0800

    Technical skill progress bar

--- production/src/sections/TechnicalSkill.tsx
+++ production/src/sections/TechnicalSkill.tsx
@@ -1,14 +1,14 @@
 import React, { forwardRef } from 'react';
-import { Box, Typography, List, ListItem, ListItemText } from '@mui/material';
+import { Box, Typography, Grid, LinearProgress } from '@mui/material';
 
 const TechnicalSkill = forwardRef<HTMLDivElement>((props, ref) => {
     const technicalSkills = [
-        'JavaScript',
-        'TypeScript',
-        'React',
-        'HTML',
-        'CSS',
-        'UI/UX Design',
+        { name: 'JavaScript', percentage: 90 },
+        { name: 'TypeScript', percentage: 85 },
+        { name: 'React', percentage: 95 },
+        { name: 'HTML', percentage: 80 },
+        { name: 'CSS', percentage: 75 },
+        { name: 'UI/UX Design', percentage: 70 },
         // Add more technical skills here
     ];
 
@@ -16,13 +16,21 @@ const TechnicalSkill = forwardRef<HTMLDivElement>((props, ref) => {
         <div ref={ref}>
             <Box>
                 <Typography variant="h4" color="primary">Technical Skills</Typography>
-                <List>
-                    {technicalSkills.map(skill => (
-                        <ListItem key={skill}>
-                            <ListItemText primary={skill} />
-                        </ListItem>
-                    ))}
-                </List>
+
+                <Grid container spacing={2}>
+
+                {technicalSkills.map(skill => (
+                    <Grid item xs={12} sm={6} md={4} key={skill.name}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography>{skill.name}</Typography>
+                        <Box sx={{ width: '100%', ml: 1 }}>
+                            <LinearProgress variant="determinate" value={skill.percentage} />
+                        </Box>
+                    </Box>
+                    </Grid>
+                ))}
+
+                </Grid>
             </Box>
         </div>
     );
